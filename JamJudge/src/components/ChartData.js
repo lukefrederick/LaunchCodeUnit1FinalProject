cf7664e4be3b8e1c710ea9ebf4af7952fc09dca2
lfrederick034 <lfrederick220@gmail.com>
2025-11-10 18:28:23 -0600

    Mock data to be used for charts added.

---
+++ JamJudge/src/components/ChartData.js
@@ -0,0 +1,57 @@
+// This is for the chart data...
+// You can probably tell I'm a loser based on my music taste
+const ChartData = [
+
+    {
+        albumName: "Getting Killed",
+        artistName: "Geese",
+        releaseDate: 2025
+    },
+    {
+        albumName: "Ghost City",
+        artistName: "Delta Sleep",
+        releaseDate: 2018
+    },
+    {
+        albumName: "Koi No Yokan",
+        artistName: "Deftones",
+        releaseDate: 2012
+    },
+    {
+        albumName: "Stratosphere",
+        artistName: "Duster",
+        releaseDate: 1998
+    },
+    {
+        albumName: "Beyond the Fleeting Gales",  
+        artistName: "Crying",
+        releaseDate: 2016
+    },
+    {
+        albumName: "D>E>A>T>H>M>E>T>A>L",
+        artistName: "Panchiko",
+        releaseDate: 2000
+    },
+    {
+        albumName: "$0",
+        artistName: "Cameron Winter",
+        releaseDate: 2024
+    },
+    {
+        albumName: "Feels Like You",
+        artistName: "Whirr",
+        releaseDate: 2019
+    },
+    {
+        albumName: "Shed",
+        artistName: "Title Fight",
+        releaseDate: 2011
+    },
+    {
+        albumName: "Twin Fantasy",
+        artistName: "Car Seat Headrest",
+        releaseDate: 2917
+    }
+]
+
+export default ChartData;
